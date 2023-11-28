004ce14de3b60957f7aa746727b7ac139630b300
fdeegbe <87954337+fdeegbe@users.noreply.github.com>
2023-11-27 20:05:28 -0500

    Added starry background

--- website_portfolio/portfolio/src/App.jsx
+++ website_portfolio/portfolio/src/App.jsx
@@ -1,14 +1,19 @@
-import React, { useState } from 'react';
-import Navbar from './components/Navbar';
+import { useCallback } from "react";
+// import Particles from "react-particles";
+//import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "tsparticles-slim"; // if you are going to use `loadSlim`, install the "tsparticles-slim" package too.
+import ParticlesContainer from "./components/ParticlesContainer";
+import Navbar from "./components/Navbar";
 
-function App() {
-  
+const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <div style={{ zIndex: 1 }}>
+        <ParticlesContainer />
+      </div>
     </div>
-
   );
-}
+};
 
-export default App;
+export default App
--- website_portfolio/portfolio/src/components/Navbar.jsx
+++ website_portfolio/portfolio/src/components/Navbar.jsx
@@ -1,31 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import Lottie from 'react-lottie';
+
 
 const Navbar = () => {
-  const [animationData, setAnimationData] = useState(null);
-
-  useEffect(() => {
-    // Fetch the Lottie JSON file
-    fetch('https://lottie.host/840209ea-da38-4c7f-9bc3-8424c854a725/KCHeu8r4M2.json')
-      .then((response) => response.json())
-      .then((data) => setAnimationData(data))
-      .catch((error) => console.error('Error fetching Lottie JSON:', error));
-  }, []);
-
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
-  };
 
   return (
-    <div className='h-screen w-[10%] lg:w-[15%] bg-primary flex flex-col justify-between'>
-      {animationData && (
-        <Lottie options={defaultOptions} height={50} width={50} />
-      )}
+    <div style={{ zIndex: 2 }} className='h-screen w-[10%] lg:w-[15%] bg-primary flex flex-col justify-between'>
+
       <div className='text-accent my-2'>
         hi (top)
       </div>
